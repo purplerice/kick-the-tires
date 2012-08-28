7a5831416ae44ec3c74884f2873d4a9fd78eda2b
Sigal Holdeman <sigalholdeman@gmail.com>
2012-08-28 05:24:35 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KickTheTires::Application.config.secret_token = 'f37de23260af64f4be59c93665a90855093e057a46114cff86c427d9954cce2fb31d9d8dee43344185a2f79d08e463dfc8314383b88660578b51f8532692ff5f'
